c5a031852ede30343e01e070368b97e965a1951e
DomenicoDErasmo <44101761+DomenicoDErasmo@users.noreply.github.com>
2023-07-31 21:17:54 -0400

    starting to implement instructions

--- src/emulator.rs
+++ src/emulator.rs
@@ -3,23 +3,19 @@ use winit::event::VirtualKeyCode;
 const FPS: f64 = 60.0;
 
 pub struct Emulator {
-    pub renderer: crate::renderer::RendererState,
-    pub event_loop: winit::event_loop::EventLoop<()>,
     memory: [u8; 4096],
     _stack: crate::stack::Stack,
     _delay_timer: crate::timer::Timer,
     _sound_timer: crate::timer::Timer,
     pressed: std::collections::HashMap<VirtualKeyCode, bool>,
     program_counter: usize,
+    index_register: usize,
+    variable_registers: [u8; 16],
+    screen: [[bool; crate::screen::SCREEN_WIDTH as usize]; crate::screen::SCREEN_HEIGHT as usize],
 }
 
 impl Emulator {
     pub async fn new() -> Self {
-        let event_loop = winit::event_loop::EventLoop::new();
-        let window = winit::window::WindowBuilder::new()
-            .build(&event_loop)
-            .unwrap();
-        let renderer = crate::renderer::RendererState::new(window).await;
         let mut memory = [0x00; 4096];
         Self::load_font(&mut memory);
 
@@ -28,6 +24,7 @@ impl Emulator {
         let sound_timer = crate::timer::Timer::new();
 
         let program_counter = 0;
+        let index_register = 0;
 
         let pressed = std::collections::HashMap::from([
             (VirtualKeyCode::Key1, false),
@@ -48,118 +45,128 @@ impl Emulator {
             (VirtualKeyCode::V, false),
         ]);
 
+        let variable_registers = [0; 16];
+
+        let screen =
+            [[false; crate::screen::SCREEN_WIDTH as usize]; crate::screen::SCREEN_HEIGHT as usize];
+
         Self {
-            event_loop,
-            renderer,
             memory,
             _stack: stack,
             _delay_timer: delay_timer,
             _sound_timer: sound_timer,
             pressed,
             program_counter,
+            index_register,
+            variable_registers,
+            screen,
         }
     }
 
     pub async fn run(mut self) {
         env_logger::init();
+        let event_loop = winit::event_loop::EventLoop::new();
+        let window: winit::window::Window = winit::window::WindowBuilder::new()
+            .build(&event_loop)
+            .unwrap();
+        let mut renderer = crate::renderer::RendererState::new(window).await;
         let mut i = 0;
         let timer_length = std::time::Duration::new(0, (1_000_000_000.0 / FPS) as u32);
-        self.event_loop
-            .run(move |event, _, control_flow| match event {
-                // wait a frame on init
-                winit::event::Event::NewEvents(winit::event::StartCause::Init) => {
-                    control_flow.set_wait_until(std::time::Instant::now() + timer_length);
-                }
-                // wait a frame
-                winit::event::Event::NewEvents(winit::event::StartCause::ResumeTimeReached {
+        event_loop.run(move |event, _, control_flow| match event {
+            // wait a frame on init
+            winit::event::Event::NewEvents(winit::event::StartCause::Init) => {
+                control_flow.set_wait_until(std::time::Instant::now() + timer_length);
+            }
+            // wait a frame
+            winit::event::Event::NewEvents(winit::event::StartCause::ResumeTimeReached {
+                ..
+            }) => {
+                control_flow.set_wait_until(std::time::Instant::now() + timer_length);
+            }
+            // resizing window, closing window, user input
+            winit::event::Event::WindowEvent {
+                window_id,
+                ref event,
+            } if window_id == renderer.window().id() => match event {
+                winit::event::WindowEvent::CloseRequested
+                | winit::event::WindowEvent::KeyboardInput {
+                    input:
+                        winit::event::KeyboardInput {
+                            state: winit::event::ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
                     ..
-                }) => {
-                    control_flow.set_wait_until(std::time::Instant::now() + timer_length);
+                } => *control_flow = winit::event_loop::ControlFlow::Exit,
+                winit::event::WindowEvent::Resized(physical_size) => {
+                    renderer.resize(*physical_size);
                 }
-                // resizing window, closing window, user input
-                winit::event::Event::WindowEvent {
-                    window_id,
-                    ref event,
-                } if window_id == self.renderer.window().id() => match event {
-                    winit::event::WindowEvent::CloseRequested
-                    | winit::event::WindowEvent::KeyboardInput {
-                        input:
-                            winit::event::KeyboardInput {
-                                state: winit::event::ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    } => *control_flow = winit::event_loop::ControlFlow::Exit,
-                    winit::event::WindowEvent::Resized(physical_size) => {
-                        self.renderer.resize(*physical_size);
+                winit::event::WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    renderer.resize(**new_inner_size);
+                }
+                winit::event::WindowEvent::KeyboardInput {
+                    input:
+                        winit::event::KeyboardInput {
+                            state: winit::event::ElementState::Pressed,
+                            virtual_keycode,
+                            ..
+                        },
+                    ..
+                } => match virtual_keycode {
+                    Some(winit::event::VirtualKeyCode::Escape) => {
+                        *control_flow = winit::event_loop::ControlFlow::Exit
                     }
-                    winit::event::WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        self.renderer.resize(**new_inner_size);
+                    Some(keycode) if self.pressed.contains_key(keycode) => {
+                        println!("{:?} was pressed", keycode);
+                        *self.pressed.get_mut(keycode).unwrap() = true;
                     }
-                    winit::event::WindowEvent::KeyboardInput {
-                        input:
-                            winit::event::KeyboardInput {
-                                state: winit::event::ElementState::Pressed,
-                                virtual_keycode,
-                                ..
-                            },
-                        ..
-                    } => match virtual_keycode {
-                        Some(winit::event::VirtualKeyCode::Escape) => {
-                            *control_flow = winit::event_loop::ControlFlow::Exit
-                        }
-                        Some(keycode) if self.pressed.contains_key(keycode) => {
-                            println!("{:?} was pressed", keycode);
-                            *self.pressed.get_mut(keycode).unwrap() = true;
-                        }
-                        _ => {}
-                    },
                     _ => {}
                 },
-                // explicit redraw request
-                winit::event::Event::RedrawRequested(window_id)
-                    if window_id == self.renderer.window().id() =>
-                {
-                    self.renderer.update();
-                    match self.renderer.render() {
-                        Ok(_) => {}
-                        Err(wgpu::SurfaceError::Lost) => self.renderer.resize(self.renderer.size),
-                        Err(wgpu::SurfaceError::OutOfMemory) => {
-                            *control_flow = winit::event_loop::ControlFlow::Exit
-                        }
-                        Err(e) => eprint!("{:?}", e),
-                    }
-                }
-                // everything else - no input or waiting
-                winit::event::Event::MainEventsCleared => {
-                    // 12x a frame -> 720 / instructions per second on 60 FPS
-                    for _ in 0..12 {
-                        // fetch
-                        let instruction_bytes: &[u8; 2] = &self.memory
-                            [self.program_counter..self.program_counter + 2]
-                            .try_into()
-                            .expect("Wrong length");
-
-                        // decode
-                        let _instruction = crate::bit_utils::append_number_bits(instruction_bytes);
-
-                        // execute
-                    }
-
-                    // timer test
-                    // TODO: remove
-                    i = i + 1;
-                    if i >= FPS as i32 {
-                        i = 0;
-                        println!("A second passed");
-                    }
-
-                    // render
-                    self.renderer.window().request_redraw();
-                }
                 _ => {}
-            })
+            },
+            // explicit redraw request
+            winit::event::Event::RedrawRequested(window_id)
+                if window_id == renderer.window().id() =>
+            {
+                renderer.update();
+                match renderer.render() {
+                    Ok(_) => {}
+                    Err(wgpu::SurfaceError::Lost) => renderer.resize(renderer.size),
+                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                        *control_flow = winit::event_loop::ControlFlow::Exit
+                    }
+                    Err(e) => eprint!("{:?}", e),
+                }
+            }
+            // everything else - no input or waiting
+            winit::event::Event::MainEventsCleared => {
+                // 12x a frame -> 720 / instructions per second on 60 FPS
+                for _ in 0..12 {
+                    // fetch
+                    let instruction_bytes: &[u8; 2] = &self.memory
+                        [self.program_counter..self.program_counter + 2]
+                        .try_into()
+                        .expect("Wrong length");
+
+                    // decode
+                    let instruction = crate::bit_utils::append_number_bits(instruction_bytes);
+                    self.parse_instruction(instruction);
+                    // execute
+                }
+
+                // timer test
+                // TODO: remove
+                i = i + 1;
+                if i >= FPS as i32 {
+                    i = 0;
+                    println!("A second passed");
+                }
+
+                // render
+                renderer.window().request_redraw();
+            }
+            _ => {}
+        })
     }
 
     fn load_font(memory: &mut [u8; 4096]) {
@@ -182,5 +189,51 @@ impl Emulator {
             0xF0, 0x80, 0xF0, 0x80, 0x80, // F
         ];
         memory[0x50..0xA0].clone_from_slice(font);
+    }
+
+    fn parse_instruction(&mut self, instruction: u16) {
+        let first_nibble = crate::bit_utils::bit_range_to_num(instruction, 0, 4).unwrap();
+        let second_nibble = crate::bit_utils::bit_range_to_num(instruction, 4, 8).unwrap();
+        let _third_nibble = crate::bit_utils::bit_range_to_num(instruction, 8, 12).unwrap();
+        let fourth_nibble = crate::bit_utils::bit_range_to_num(instruction, 12, 16).unwrap();
+        let nibbles_2_to_4 = crate::bit_utils::bit_range_to_num(instruction, 4, 16).unwrap();
+        let nibbles_3_to_4 = crate::bit_utils::bit_range_to_num(instruction, 8, 16).unwrap();
+
+        match first_nibble {
+            0x0 => match fourth_nibble {
+                0x0 => self.clear_screen(),
+                0xE => {}
+                _ => {}
+            },
+            0x1 => self.program_counter = nibbles_2_to_4.into(),
+            0x2 => {}
+            0x3 => {}
+            0x4 => {}
+            0x5 => {}
+            0x6 => self.variable_registers[second_nibble as usize] = nibbles_3_to_4 as u8,
+            0x7 => {
+                // normally we would check if <= 255 but Rust does this for us
+                let register_to_change = &mut self.variable_registers[second_nibble as usize];
+                let candidate_sum = *register_to_change + (nibbles_3_to_4 as u8);
+                *register_to_change = candidate_sum;
+            }
+            0x8 => {}
+            0x9 => {}
+            0xA => self.index_register = nibbles_3_to_4.into(),
+            0xB => {}
+            0xC => {}
+            0xD => {}
+            0xE => {}
+            0xF => {}
+            _ => {}
+        }
+    }
+
+    fn clear_screen(&mut self) {
+        for row in self.screen.iter_mut() {
+            for cell in row.iter_mut() {
+                *cell = false;
+            }
+        }
     }
 }
