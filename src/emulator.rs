31c647c040d49157fce9556f70c837e6cf3f4b5a
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2023-08-27 14:43:19 -0400

    removed unused function and library

--- src/emulator.rs
+++ src/emulator.rs
@@ -223,20 +223,6 @@ impl Emulator {
         (sink, source)
     }
 
-    /// Loads an icon for the emulator from the specified path.
-    fn load_icon(path: &std::path::Path) -> winit::window::Icon {
-        let (icon_rgba, icon_width, icon_height) = {
-            let image = image::open(path)
-                .expect("Failed to open icon path")
-                .into_rgba8();
-            let (width, height) = image.dimensions();
-            let rgba = image.into_raw();
-            (rgba, width, height)
-        };
-        winit::window::Icon::from_rgba(icon_rgba, icon_width, icon_height)
-            .expect("Failed to open icon")
-    }
-
     /// Loads the current instructions from memory.
     fn get_instructions_from_memory(&mut self) -> [u8; 2] {
         self.memory[self.program_counter..self.program_counter + 2]
